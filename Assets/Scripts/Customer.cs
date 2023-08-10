f8d5beba2e3c64eed1c2dc614cef2e812c98b35e
Hüseyin Koçer <huso1475@msn.com>
2023-08-10 13:17:30 +0300

    Implemented customer chasing behavior.

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -6,6 +6,33 @@ namespace nopact.ChefsLastStand.Gameplay.Entities
 {
     public class Customer : MonoBehaviour
     {
-        
+        [SerializeField] protected float moveSpeed = 1f;
+
+        private Transform chefTransform;
+
+        protected virtual void Start()
+        {
+            GameObject chefObject = GameObject.FindWithTag("Chef");
+            if (chefObject != null)
+            {
+                chefTransform = chefObject.transform;
+            }
+            else
+            {
+                Debug.LogError("Chef object not found. Make sure the Chef is tagged appropriately.");
+            }
+        }
+        protected virtual void Update()
+        {
+            ChaseChef();
+        }
+
+        protected virtual void ChaseChef()
+        {
+            if (chefTransform == null) return;
+
+            Vector2 directionToChef = (chefTransform.position - transform.position).normalized;
+            transform.position += (Vector3)directionToChef * moveSpeed * Time.deltaTime;
+        }
     }
 }
